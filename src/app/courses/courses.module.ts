90c195de98fa482885dd9843c5baeccc8bd18e4b
Magno Leal de Brito Junior <CIANDT\magnobrito@lnb024243cps.ciandt.global>
2023-06-06 07:56:11 -0300

    First commit

---
+++ src/app/courses/courses.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+import { CoursesRoutingModule } from './courses-routing.module';
+import { CoursesComponent } from './courses/courses.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    CoursesComponent
+  ],
+  imports: [
+    CommonModule,
+    CoursesRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
+  ]
+})
+export class CoursesModule { }
